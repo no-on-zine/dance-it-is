1116a01475799fae24a519aeffd1fd12a6ce5743
no-on-zine <60006483+no-on-zine@users.noreply.github.com>
2020-09-19 14:25:45 +0900

    0919 1425

--- footer.php
+++ footer.php
@@ -1,5 +1,114 @@
 <section id="f_menu">
     <div class="c_wrap">
+        <div class="nav_w">
+            <ul class="pc_nav">
+                <li>
+                    <a href="<?php echo home_url() ?>/dii2020/about">ABOUT</a>
+                </li>
+                <li>｜</li>
+                <li>
+                    <a href="<?php echo home_url() ?>/dii2020/platform">PLATFORM</a>
+                </li>
+                <li>｜</li>
+                <li>
+                    <a href="<?php echo home_url() ?>/dii2020/program">PROGRAM</a>
+                </li>
+                <li>｜</li>
+                <li>
+                    <a href="<?php echo home_url() ?>/dii2020/schedule">SCHEDULE</a>
+                </li>
+                <li>｜</li>
+                <li>
+                    <a href="<?php echo home_url() ?>/magazine">MAGAZINE</a>
+                </li>
+                <li>｜</li>
+                <li>
+                    <a href="<?php echo home_url() ?>/archive">ARCHIVE</a>
+                </li>
+            </ul>
+            <div class="menu_l">
+                <img src="<?php bloginfo('template_url'); ?>/images/common/menu_l.png">
+            </div>
+        </div>
+        <div class="menu_w">
+            <ul>
+                <li>
+                    <a href="#">プロジェクトについて</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="#">Sapporo Choreo振付家養成講座</a>
+                </li>
+                <li>
+                    <a href="#">レジデンスアーティスト育成事業2021</a>
+                </li>
+                <li>
+                    <a href="#">Dance Camp クリエイション＆ダイアローグ・ワークショップ</a>
+                </li>
+                <li>
+                    <a href="#">DANCE ARTIST VIEW2020「セルフカルチベート企画」</a>
+                </li>
+                <li>
+                    <a href="#">ダンスアートプロジェクト！！</a>
+                </li>
+                <li>
+                    <a href="#">建築とコンタクト（京都‒札幌）</a>
+                </li>
+                <li>
+                    <a href="#">わたしと身体の緩やかなダンス革命 in 福岡</a>
+                </li>
+                <li>
+                    <a href="#">「役立つ」バレエ・ダンス史</a>
+                </li>
+                <li>
+                    <a href="#">第4回「ダンスを撮る！」ダンス映像撮影ワークショップ</a>
+                </li>
+                <li>
+                    <a href="#">KYOTO Dance Platform（仮）</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="#">年間スケジュール</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="#">マガジン一覧</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="#">Dance it is!! 2019</a>
+                </li>
+                <li>
+                    <a href="#">報告書ダウンロード</a>
+                </li>
+                <li>
+                    <a href="#">記録映像</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div clss="info_1">
+        文化庁委託事業「令和2年度次代の文化を創造する新進芸術家育成事業」<br> 主催：文化庁／NPO法人ジャパン・コンテンポラリーダンス・ネットワーク（JCDN）
+        <br> 各地共催・制作・協力：北海道コンテンポラリーダンス普及委員会／ダンスハウス黄金4442／Dance Camp Project／<br> 城崎国際アートセンター（豊岡市）／NPO法人DANCE BOX／FREE HEARTS／広島市安芸区民文化センター／C3/Contact Choreograph Crossing／<br> 一般社団法人ダンスアンドエンヴァイロメント／micelle／あけぼのアート&コミュニティーセンター（札幌）／ボディ・アーツ・ラボラトリー／
+        <br> NPO法人コデックス／ダンスヒストリー・スタディーズ／Dance New Air（一般社団法人ダンス・ニッポン・アソシエイツ）／<br> 京都芸術センター（Co-programカテゴリーD「KACセレクション」採択企画）
+    </div>
+    <div class="info_2">
+        事務局：NPO法人ジャパン・コンテンポラリーダンス・ネットワーク（JCDN）<br> 統括：佐東範一　運営：神前沙織、榊原愛
+        <br> 〒600-8092 京都府京都市下京区神明町241オパス四条503 <br> Tel: 075-361-4685 Fax: 075-361-6225 Web: http://www.jcdn.org
+    </div>
+    <div class="info_3">
+        <ul>
+            <li>
+                <img src="<?php bloginfo('template_url'); ?>/images/common/f_1.png" alt="文化庁">
+            </li>
+            <li>
+                <img src="<?php bloginfo('template_url'); ?>/images/common/f_2.png" alt="NPO法人ジャパン・コンテンポラリーダンス・ネットワーク（JCDN）">
+            </li>
+        </ul>
     </div>
 </section>
 
