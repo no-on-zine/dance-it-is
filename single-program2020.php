4e1e658b4cac57a2a9fad4474d48714543bcc18a
no-on-zine <60006483+no-on-zine@users.noreply.github.com>
2020-09-23 21:42:33 +0900

    Keita
    0923 2142

--- single-program2020.php
+++ single-program2020.php
@@ -9,6 +9,9 @@ get_header(); ?>
     <section id="m_v">
         <div class="w_box"></div>
         <div class="c_wrap">
+            <div class="thum">
+                <?php the_post_thumbnail(); ?>
+            </div>
             <div class="title_area">
                 <div class="<?php the_field('p_status'); ?>">
                     <ul>
@@ -144,6 +147,7 @@ get_header(); ?>
                     <p>/募集概要</p>
                 </div>
                 <?php endif;?>
+
                 <?php $p_name_2 = get_field('p_name_2');?>
                 <?php if(empty($p_name_2)):?>
 
@@ -153,73 +157,249 @@ get_header(); ?>
                     <p>/募集概要</p>
                 </div>
                 <?php endif;?>
+
+                <?php $p_name_3 = get_field('p_name_3');?>
+                <?php if(empty($p_name_3)):?>
+
+                <?php else:?>
+                <div class="p_name_3 flex_items box_3">
+                    <?php echo $p_name_3; ?>
+                    <p>/募集概要</p>
+                </div>
+                <?php endif;?>
+
+                <?php $p_name_4 = get_field('p_name_4');?>
+                <?php if(empty($p_name_4)):?>
+
+                <?php else:?>
+                <div class="p_name_4 flex_items box_4">
+                    <?php echo $p_name_4; ?>
+                    <p>/募集概要</p>
+                </div>
+                <?php endif;?>
+
+                <?php $p_name_5 = get_field('p_name_5');?>
+                <?php if(empty($p_name_5)):?>
+
+                <?php else:?>
+                <div class="p_name_5 flex_items box_5">
+                    <?php echo $p_name_5; ?>
+                    <p>/募集概要</p>
+                </div>
+                <?php endif;?>
             </div>
 
             <div class="contents">
                 <div class="box_1">
-                    <?php $p_terms_1 = get_field('p_terms_1');?>
-                    <?php if(empty($p_terms_1)):?>
-                    <?php else:?>
-                    <h5>［募集条件］</h5>
-                    <p>
-                        <?php echo $p_terms_1; ?>
-                    </p>
-                    <?php endif;?>
+                    <div class="left">
+                        <?php $p_notice_1 = get_field('p_notice_1');?>
+                        <?php if(empty($p_notice_1)):?>
+                        <?php else:?>
+                        <div class="f_w">
+                            <h5><?php echo $p_notice_1; ?></h5>
+                        </div>
+                        <?php endif;?>
 
-                    <?php $p_date_detail_1 = get_field('p_date_detail_1');?>
-                    <?php if(empty($p_date_detail_1)):?>
-                    <?php else:?>
-                    <h5>［日程詳細］</h5>
-                    <p>
-                        <?php echo $p_date_detail_1; ?>
-                    </p>
-                    <?php endif;?>
+                        <?php $p_terms_1 = get_field('p_terms_1');?>
+                        <?php if(empty($p_terms_1)):?>
+                        <?php else:?>
+                        <div class="f_w">
+                            <h5>［募集条件］</h5>
+                            <p>
+                                <?php echo $p_terms_1; ?>
+                            </p>
+                        </div>
+                        <?php endif;?>
 
-                    <?php $p_schedule_1 = get_field('p_schedule_1');?>
-                    <?php if(empty($p_schedule_1)):?>
-                    <?php else:?>
-                    <h5>［プロジェクト全体スケジュール］</h5>
-                    <p>
-                        <?php echo $p_schedule_1; ?>
-                    </p>
-                    <?php endif;?>
+                        <?php $p_date_detail_1 = get_field('p_date_detail_1');?>
+                        <?php if(empty($p_date_detail_1)):?>
+                        <?php else:?>
+                        <div class="f_w">
+                            <h5>［日程詳細］</h5>
+                            <p>
+                                <?php echo $p_date_detail_1; ?>
+                            </p>
+                        </div>
+                        <?php endif;?>
 
-                    <?php $p_lecturer_1 = get_field('p_lecturer_1');?>
-                    <?php if(empty($p_lecturer_1)):?>
-                    <?php else:?>
-                    <h5>［講師］</h5>
-                    <p>
-                        <?php echo $p_lecturer_1; ?>
-                    </p>
-                    <?php endif;?>
+                        <?php $p_schedule_1 = get_field('p_schedule_1');?>
+                        <?php if(empty($p_schedule_1)):?>
+                        <?php else:?>
+                        <div class="f_w">
+                            <h5>［プロジェクト全体スケジュール］</h5>
+                            <p>
+                                <?php echo $p_schedule_1; ?>
+                            </p>
+                        </div>
+                        <?php endif;?>
 
-                    <?php $p_shooting_1 = get_field('p_shooting_1');?>
-                    <?php if(empty($p_shooting_1)):?>
-                    <?php else:?>
-                    <h5>［撮影］</h5>
-                    <p>
-                        <?php echo $p_shooting_1; ?>
-                    </p>
-                    <?php endif;?>
+                        <?php $p_lecturer_1 = get_field('p_lecturer_1');?>
+                        <?php if(empty($p_lecturer_1)):?>
+                        <?php else:?>
+                        <div class="f_w">
+                            <h5>［講師］</h5>
+                            <p>
+                                <?php echo $p_lecturer_1; ?>
+                            </p>
+                        </div>
+                        <?php endif;?>
 
-                    <?php $p_fee_1 = get_field('p_fee_1');?>
-                    <?php if(empty($p_fee_1)):?>
-                    <?php else:?>
-                    <h5>［参加費］</h5>
-                    <p>
-                        <?php echo $p_fee_1; ?>
-                    </p>
-                    <?php endif;?>
+                        <?php $p_shooting_1 = get_field('p_shooting_1');?>
+                        <?php if(empty($p_shooting_1)):?>
+                        <?php else:?>
+                        <div class="f_w">
+                            <h5>［撮影］</h5>
+                            <p>
+                                <?php echo $p_shooting_1; ?>
+                            </p>
+                        </div>
+                        <?php endif;?>
 
-                    <?php $p_capacity_1 = get_field('p_capacity_1');?>
-                    <?php if(empty($p_capacity_1)):?>
-                    <?php else:?>
-                    <h5>［定員］</h5>
-                    <p>
-                        <?php echo $p_capacity_1; ?>
-                    </p>
-                    <?php endif;?>
+                        <?php $p_fee_1 = get_field('p_fee_1');?>
+                        <?php if(empty($p_fee_1)):?>
+                        <?php else:?>
+                        <div class="f_w">
+                            <h5>［参加費］</h5>
+                            <p>
+                                <?php echo $p_fee_1; ?>
+                            </p>
+                        </div>
+                        <?php endif;?>
 
+                        <?php $p_capacity_1 = get_field('p_capacity_1');?>
+                        <?php if(empty($p_capacity_1)):?>
+                        <?php else:?>
+                        <div class="f_w">
+                            <h5>［定員］</h5>
+                            <p>
+                                <?php echo $p_capacity_1; ?>
+                            </p>
+                        </div>
+                        <?php endif;?>
+
+                        <?php $p_application_m_1 = get_field('p_application_m_1');?>
+                        <?php if(empty($p_application_m_1)):?>
+                        <?php else:?>
+                        <div class="f_w">
+                            <h5>［応募方法］</h5>
+                            <p>
+                                <?php echo $p_application_m_1; ?>
+                            </p>
+                        </div>
+                        <?php endif;?>
+
+                        <?php $p_period_1 = get_field('p_period_1');?>
+                        <?php if(empty($p_period_1)):?>
+                        <?php else:?>
+                        <div class="f_w">
+                            <h5>［募集期間］</h5>
+                            <p>
+                                <?php echo $p_period_1; ?>
+                            </p>
+                        </div>
+                        <?php endif;?>
+
+                        <?php $p_way_1 = get_field('p_way_1');?>
+                        <?php if(empty($p_way_1)):?>
+                        <?php else:?>
+                        <div class="f_w">
+                            <h5>［方法］</h5>
+                            <p>
+                                <?php echo $p_way_1; ?>
+                            </p>
+                        </div>
+                        <?php endif;?>
+
+                        <?php $p_target_1 = get_field('p_target_1');?>
+                        <?php if(empty($p_target_1)):?>
+                        <?php else:?>
+                        <div class="f_w">
+                            <h5>［対象］</h5>
+                            <p>
+                                <?php echo $p_target_1; ?>
+                            </p>
+                        </div>
+                        <?php endif;?>
+
+                        <?php $p_inquiry_1 = get_field('p_inquiry_1');?>
+                        <?php if(empty($p_inquiry_1)):?>
+                        <?php else:?>
+                        <div class="f_w inquiry">
+                            <h5>問合・申込</h5>
+                            <p>
+                                <?php echo $p_inquiry_1; ?>
+                            </p>
+                        </div>
+                        <?php endif;?>
+                    </div>
+                    <div class="right">
+                        <div class="lecturer">
+                            <?php $p_l_photo_1_1 = get_field('p_l_photo_1_1');?>
+                            <?php if(empty($p_l_photo_1_1)):?>
+                            <?php else:?>
+                            <div class="i_w">
+                                <img src="<?php echo $p_l_photo_1_1; ?>">
+                            </div>
+                            <?php endif;?>
+
+                            <?php $p_l_name_1_1 = get_field('p_l_name_1_1');?>
+                            <?php if(empty($p_l_name_1_1)):?>
+                            <?php else:?>
+                            <h5><?php echo $p_l_name_1_1; ?></h5>
+                            <?php endif;?>
+
+                            <?php $p_l_profile_1_1 = get_field('p_l_profile_1_1');?>
+                            <?php if(empty($p_l_profile_1_1)):?>
+                            <?php else:?>
+                            <p><?php echo $p_l_profile_1_1; ?></p>
+                            <?php endif;?>
+                        </div>
+
+                        <div class="lecturer">
+                            <?php $p_l_photo_2_1 = get_field('p_l_photo_2_1');?>
+                            <?php if(empty($p_l_photo_2_1)):?>
+                            <?php else:?>
+                            <div class="i_w">
+                                <img src="<?php echo $p_l_photo_2_1; ?>">
+                            </div>
+                            <?php endif;?>
+
+                            <?php $p_l_name_2_1 = get_field('p_l_name_2_1');?>
+                            <?php if(empty($p_l_name_2_1)):?>
+                            <?php else:?>
+                            <h5><?php echo $p_l_name_2_1; ?></h5>
+                            <?php endif;?>
+
+                            <?php $p_l_profile_2_1 = get_field('p_l_profile_2_1');?>
+                            <?php if(empty($p_l_profile_2_1)):?>
+                            <?php else:?>
+                            <p><?php echo $p_l_profile_2_1; ?></p>
+                            <?php endif;?>
+                        </div>
+
+                        <div class="lecturer">
+                            <?php $p_l_photo_3_1 = get_field('p_l_photo_3_1');?>
+                            <?php if(empty($p_l_photo_3_1)):?>
+                            <?php else:?>
+                            <div class="i_w">
+                                <img src="<?php echo $p_l_photo_3_1; ?>">
+                            </div>
+                            <?php endif;?>
+
+                            <?php $p_l_name_3_1 = get_field('p_l_name_3_1');?>
+                            <?php if(empty($p_l_name_3_1)):?>
+                            <?php else:?>
+                            <h5><?php echo $p_l_name_3_1; ?></h5>
+                            <?php endif;?>
+
+                            <?php $p_l_profile_3_1 = get_field('p_l_profile_3_1');?>
+                            <?php if(empty($p_l_profile_3_1)):?>
+                            <?php else:?>
+                            <p><?php echo $p_l_profile_3_1; ?></p>
+                            <?php endif;?>
+                        </div>
+
+                    </div>
                 </div>
             </div>
 
